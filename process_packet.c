6783dc67b18b487c0f14ca4692bc3bbd938e8644
IgorShalyg <igor.s@standuply.com>
2020-06-18 23:42:11 +0700

    fix modified files

--- process_packet.c
+++ process_packet.c
@@ -1,4 +1,43 @@
-//
-// Created by user on 16.06.2020.
-//
+#include <stdio.h>
+#include <netinet/ip.h>
+#include <netinet/ether.h>
+#include <net/if_arp.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <linux/if_ether.h>
+#include <net/ethernet.h>
 
+void process_ipv4(const struct ip*);
+void process_arp(const struct arphdr*);
+void process_ipv6();
+
+void process_packet(u_char* _, const struct pcap_pkthdr* hdr, const u_char* packet) {
+    const struct ether_header *eth_ptr = (struct ether_header*)(packet);
+    uint16_t type = ntohs(eth_ptr->ether_type);
+    const u_char* payload = (ETHER_HDR_LEN + packet);
+
+    switch (type) {
+        case ETHERTYPE_IP:
+            process_ipv4((const struct ip*)payload);
+            break;
+        case ETHERTYPE_ARP:
+              printf("Nope\n");
+//            process_arp(ETH_HLEN + packet);
+            break;
+        case ETHERTYPE_REVARP:
+            // TODO
+            break;
+        case ETHERTYPE_IPV6:
+            // TODO
+            break;
+        default:
+            printf("Currently there's no support for %d ether_type", type);
+            return;
+    }
+}
+
+void process_ipv4(const struct ip* packet) {
+    printf("Total packet length %d  |  ", packet->ip_len);
+    printf("%s -----> %s\n", inet_ntoa(packet->ip_src), inet_ntoa(packet->ip_dst));
+
+}
--- process_packet.h
+++ process_packet.h
@@ -5,4 +5,6 @@
 #ifndef CAPTURE_PROCESS_PACKET_H
 #define CAPTURE_PROCESS_PACKET_H
 
+void process_packet(u_char* _, const struct pcap_pkthdr* hdr, const u_char* packet);
+
 #endif //CAPTURE_PROCESS_PACKET_H
