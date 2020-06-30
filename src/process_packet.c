1b7c3676767f637b2a33c284225c3c65d843171f
IgorShalyg <igor.s@standuply.com>
2020-06-30 22:21:46 +0700

    Add todos for other protocols, remove unsued includes, fix garbage tcp data printing

--- src/process_packet.c
+++ src/process_packet.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 #include <string.h>
-#include <ncurses.h>
+
+#include <inttypes.h>
 
 #include <netinet/ip.h>
 #include <netinet/ether.h>
 #include <netinet/tcp.h>
 #include <netinet/udp.h>
 #include <net/if_arp.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
 #include <linux/if_ether.h>
 #include <net/ethernet.h>
 
 #include "utils/stdout.h"
 
-void process_ipv4(const struct ip*);
+void process_ipv4(const u_char*);
 void process_ipv6(); // TODO
 
 void print_eth_hdr(const struct ether_header*);
 void print_arp(const struct arphdr*); // TODO
 void print_ipv4_hdr(const struct iphdr*);
-void print_tcp(const struct tcphdr*);
-void print_udp(const struct udphdr*);
+void print_tcp(const u_char*, const int);
+void print_udp(const u_char*); // TODO
 
-FILE* logfile;
+void print_data(const unsigned char *buffer, const unsigned int len); // TODO
+
+FILE* logfile; // TODO
 struct sockaddr_in source, dest;
 
 void process_packet(u_char* _, const struct pcap_pkthdr* hdr, const u_char* buffer) {
@@ -35,10 +37,9 @@ void process_packet(u_char* _, const struct pcap_pkthdr* hdr, const u_char* buff
 
     switch (type) {
         case ETHERTYPE_IP:
-            process_ipv4((const struct ip*)payload);
+            process_ipv4(payload);
             break;
         case ETHERTYPE_ARP:
-              printf("Nope\n");
 //            print_arp(ETH_HLEN + packet);
             break;
         case ETHERTYPE_REVARP:
@@ -55,16 +56,18 @@ void process_packet(u_char* _, const struct pcap_pkthdr* hdr, const u_char* buff
 //    printf("") // TODO print overall stats
 }
 
-void process_ipv4(const struct ip* packet) {
-    unsigned int hdr_size = packet->ip_hl * 4;
+void process_ipv4(const u_char* packet) {
+    struct iphdr* ip_hdr = (struct iphdr*)(packet);
+    unsigned int hdr_size = ip_hdr->ihl * 4;
+    u_char* payload = packet + hdr_size;
 
-    print_ipv4_hdr((struct iphdr *) (packet));
+    print_ipv4_hdr(ip_hdr);
 
-    switch (packet->ip_p) {
+    switch (ip_hdr->protocol) {
         case IPPROTO_ICMP:
             break;
         case IPPROTO_TCP:
-            print_tcp((struct tcphdr*)(packet + hdr_size));
+            print_tcp(payload, hdr_size);
             break;
         case IPPROTO_UDP:
             break;
@@ -102,12 +105,16 @@ void print_ipv4_hdr(const struct iphdr* hdr) {
     printf("\n");
 }
 
-void print_tcp(const struct tcphdr* tcp) {
-    printf("  4L  |  TCP: port(%u ->", ntohs(tcp->source));
-    printf(" %u)  ", ntohs(tcp->dest));
-    printf("seq=%d ", ntohl(tcp->seq));
-    printf("ack=%d  ", ntohl(tcp->ack_seq));
-    printf("[CWR=%d, ECN-Echo=%d, URG=%d, ACK=%d, PSH=%d, RST=%d, SYN=%d, FIN=%d]  ",
+void print_tcp(const u_char* tcp_buf, int ip_header_size) {
+    struct tcphdr* tcp = (struct tcphdr*)tcp_buf;
+
+    printf("  4L  |  TCP: port(%d ->", ntohs(tcp->th_sport));
+    printf(" %d)  ", ntohs(tcp->th_dport));
+    printf("seq=");
+    printf("%" PRIu32, ntohl(tcp->th_seq));
+    printf(" ack=");
+    printf("%" PRIu32, ntohl(tcp->th_ack));
+    printf("  [CWR=%d, ECN-Echo=%d, URG=%d, ACK=%d, PSH=%d, RST=%d, SYN=%d, FIN=%d]  ",
            (tcp->th_flags & 8) ? 1 : 0,
            (tcp->th_flags & 9) ? 1 : 0,
            (unsigned int)tcp->urg,
@@ -120,6 +127,34 @@ void print_tcp(const struct tcphdr* tcp) {
     printf("win=%d  ", ntohs(tcp->window));
     printf("checksum=%d", ntohs(tcp->check));
     printf("\n");
+
+//    print_data(tcp) // TODO
+
     printf(" ------");
     printf("\n\n");
+}
+
+void print_data(const unsigned char *data_buffer, const unsigned int length) {
+
+    printf("\t\tPayload: (%d bytes)\n\n", length - 32);
+    unsigned char byte;
+    unsigned int i, j;
+
+    for (i = 0; i < length; i++) {
+        byte = data_buffer[i];
+        printf("%02x", data_buffer[i]);
+        if (((i % 16) == 15) || (i == length - 1)) {
+            for (j = 0; j < 15 - (i % 16); j++)
+                printf("  ");
+            printf("| ");
+            for (j = (i - (i % 16)); j <= i; j++) {
+                byte = data_buffer[j];
+                if ((byte > 31) && (byte < 127))
+                    printf("%c", byte);
+                else
+                    printf(".");
+            }
+            printf("\n");
+        }
+    }
 }
