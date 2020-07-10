c379c195a47b21bb8d06fcd4b6a0cc88627b6006
IgorShalyg <igor.s@standuply.com>
2020-07-10 23:36:21 +0700

    delete dumb utils folder

--- src/utils/stdout.h
+++
@@ -1,9 +0,0 @@
-#ifndef CAPTURE_PRINT_C_H
-#define CAPTURE_PRINT_C_H
-
-void reset_color();
-void set_stdout_color(const char* color);
-void print_char(const char*, int n);
-
-
-#endif //CAPTURE_PRINT_C_H
